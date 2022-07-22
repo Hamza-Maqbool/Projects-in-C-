a552fc52835e0b2c57e2b8fab336f129e00bf146
Muhammad Hamza <hamzamaqbool722@gmail.com>
2022-07-22 11:07:02 -0700

    Windows Forms

---
+++ Space Shooter/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Space_Shooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.Image = global::Space_Shooter.Properties.Resources.player;
+            this.Player.Location = new System.Drawing.Point(458, 602);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(101, 83);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1056, 711);
+            this.Controls.Add(this.Player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Space Shooter/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EZInput;
+
+namespace Space_Shooter
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> firelist = new List<PictureBox>();
+        PictureBox EnemyBlack;
+        PictureBox EnemyRed;
+        string blackDirection = "MovingRight";
+        string redDirection = "MovingTop";
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (Keyboard.IsKeyPressed(Key.RightArrow))
+            {
+                Player.Left = Player.Left + 3;
+            }
+            if (Keyboard.IsKeyPressed(Key.LeftArrow))
+            {
+                Player.Left = Player.Left - 3;
+            }
+            if (Keyboard.IsKeyPressed(Key.Space))
+            {
+                fireBullet();
+            }
+            foreach(PictureBox bullent in firelist)
+            {
+                bullent.Top = bullent.Top - 20;
+            }
+            for(int i = 0; i < firelist.Count; i++)
+            {
+                if (firelist[i].Bottom < 0)
+                {
+                    firelist.Remove(firelist[i]);
+                }
+            }
+            moveEnemy(EnemyRed, ref redDirection);
+            moveEnemy(EnemyBlack, ref blackDirection);
+        }
+        private void fireBullet()
+        {
+            PictureBox fire = new PictureBox();
+            Image fireImge = Space_Shooter.Properties.Resources.laserGreen;
+            fire.Image = fireImge;
+            fire.Width = fireImge.Width;
+            fire.Height = fireImge.Height;
+            fire.BackColor = Color.Transparent;
+            System.Drawing.Point firelocation = new System.Drawing.Point();
+            firelocation.X = Player.Left + (Player.Width / 2) - 5;
+            firelocation.Y = Player.Top - 40;
+            fire.Location = firelocation;
+            firelist.Add(fire);
+            this.Controls.Add(fire);
+        }
+        private PictureBox createEnemy(Image img)
+        {
+            PictureBox pbenemy = new PictureBox();
+            int left = rand.Next(30, this.Width);
+            int top = rand.Next(5, img.Height + 20);
+            pbenemy.Left = left;
+            pbenemy.Top = top;
+            pbenemy.Height = img.Height;
+            pbenemy.Width = img.Width;
+            pbenemy.BackColor = Color.Transparent;
+            pbenemy.Image = img;
+            return pbenemy;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EnemyBlack = createEnemy(Space_Shooter.Properties.Resources.enemyShip);
+            EnemyRed = createEnemy(Space_Shooter.Properties.Resources.meteorBig);
+            this.Controls.Add(EnemyBlack);
+            this.Controls.Add(EnemyRed);
+
+        }
+        private void moveEnemy(PictureBox enemy,ref string direction)
+        {
+            if(direction == "MovingRight")
+            {
+                enemy.Left = enemy.Left + 10;
+            }
+            if (direction == "MovingLeft")
+            {
+                enemy.Left = enemy.Left - 10;
+            }
+            if (direction == "MovingTop")
+            {
+                enemy.Top = enemy.Top + 10;
+            }
+            if(enemy.Left + enemy.Width > this.Width)
+            {
+                direction = "MovingLeft";
+            }
+            if (enemy.Left <= 2)
+            {
+                direction = "MovingRight";
+            }
+            if(enemy.Bottom > this.Bottom)
+            {
+                EnemyRed = createEnemy(Space_Shooter.Properties.Resources.meteorBig);
+                this.Controls.Add(EnemyRed);
+            }
+        }
+    }
+}
